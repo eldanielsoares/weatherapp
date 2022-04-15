5fd4d8e92c5de75478c1daffcf2358e2c13a3fbe
eldanielsoares <danielsoares7717@gmail.com>
2022-04-15 13:39:03 -0300

    fix api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `https://api.hgbrasil.com/weather?key=XXX`,
+  baseURL: `https://api.hgbrasil.com/weather?key=XXXX`,
 });
 
 export default api;
